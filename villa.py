ecc7861ab2cbcc12f242c3d64d52ae4805e4c84a
Esha130 <eshagowani@gmail.com>
2024-11-20 09:59:29 +0530

    Create villa.py

---
+++ villa.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
